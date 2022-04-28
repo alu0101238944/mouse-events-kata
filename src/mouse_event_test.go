a5153b02506aab9e098d45cc9b3f6d2aa3dbd1e2
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-28 14:27:58 +0100

    Red: Should call all the listeners with the triple click event when a triple click occurs

--- src/mouse_event_test.go
+++ src/mouse_event_test.go
@@ -41,4 +41,23 @@ func TestMouse(t *testing.T) {
 			t.Errorf("DoubleClick didn't happen when it should\nActual: %d | Expected: %d", actual, DoubleClick)
 		}
 	})
+
+	t.Run("should call all the listeners with the triple click event when a triple click occurs", func(T *testing.T) {
+		listener := mockListener{Drop}
+		mouse.Subscribe(&listener)
+
+		mouse.PressLeftButton(0)
+		mouse.ReleaseLeftButton(100)
+
+		mouse.PressLeftButton(200)
+		mouse.ReleaseLeftButton(300)
+
+		mouse.PressLeftButton(400)
+		mouse.ReleaseLeftButton(500)
+
+		actual := listener.lastEvent
+		if actual != TripleClick {
+			t.Errorf("TripleClick didn't happen when it should\nActual: %d | Expected: %d", actual, TripleClick)
+		}
+	})
 }
