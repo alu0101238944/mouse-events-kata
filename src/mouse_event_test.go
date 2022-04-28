0ce7fc8ea43a3be63eb14c9710213f20d8430797
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-28 14:12:04 +0100

    Red: Should call all the listeners with the double click event when a double click occurs

--- src/mouse_event_test.go
+++ src/mouse_event_test.go
@@ -25,4 +25,20 @@ func TestMouse(t *testing.T) {
 			t.Errorf("Click didn't happen when it should\nActual: %d | Expected: %d", actual, Click)
 		}
 	})
+
+	t.Run("should call all the listeners with the double click event when a double click occurs", func(T *testing.T) {
+		listener := mockListener{Drop}
+		mouse.Subscribe(&listener)
+
+		mouse.PressLeftButton(0)
+		mouse.ReleaseLeftButton(100)
+
+		mouse.PressLeftButton(200)
+		mouse.ReleaseLeftButton(300)
+
+		actual := listener.lastEvent
+		if actual != DoubleClick {
+			t.Errorf("DoubleClick didn't happen when it should\nActual: %d | Expected: %d", actual, DoubleClick)
+		}
+	})
 }
